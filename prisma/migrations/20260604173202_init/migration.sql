fba34310d8fe7945848a679c8b0745e93c199ad9
Hannes <Hannes@Mac-mini.fritz.box>
2026-06-04 19:32:18 +0200

    Complete schema reset

---
+++ prisma/migrations/20260604173202_init/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "StudentProfile" ADD COLUMN     "name" TEXT;
+
+-- AlterTable
+ALTER TABLE "TutorProfile" ADD COLUMN     "name" TEXT;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "name" TEXT;
